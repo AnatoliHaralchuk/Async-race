82331bd6459ec77bbe9f4f12053681ce2c0b9bd5
AnatoliHaralchuk <sshades@mail.ru>
2022-08-05 15:54:23 +0300

    feat: render garage

--- client/src/components/view/View.ts
+++ client/src/components/view/View.ts
@@ -1,3 +1,19 @@
 export class View {
   //   constructor() {}
+  drawHeaderBtn() {
+    return `
+      <header class="header-btn">
+          <button class="header-btn__garage btn">To Garage</button>
+          <button class="header-btn__winners btn">To Winners</button>
+      </header>`
+  }
+
+  drawFooterBtn() {
+    return `
+      <footer class="footer-btn">
+          <button class="footer-btn__prev btn">prev</button>
+          <button class="footer-btn__next btn">next</button>
+      </footer>`
+  }
+
 }
--- client/src/components/view/ViewGarage.ts
+++ client/src/components/view/ViewGarage.ts
@@ -1,1 +1,25 @@
-export class ViewGarage {}
+export class ViewGarage {
+    drawSetControl(){
+        return `
+        <div class="set-control">
+            <form action="#" class="set-create form">
+                <input type="text" class="create-name input" name="name">
+                <input type="color" value="#000000" class="create-color color" name="name">
+                <button type="submit" class="create-btn btn">Create</button>
+            </form>
+            <form action="#" class="set-update form">
+                <input type="text" class="update-name input" name="name">
+                <input type="color" value="#000000" class="update-color color" name="name">
+                <button type="submit" class="update-btn btn">Update</button>
+            </form>
+        </div>`
+    }
+    drawRaceControl(){
+        return `            
+        <div class="race-control">
+            <button class="race-btn button">Race</button>
+            <button class="reset-btn button">Reset</button>
+            <button class="generate-btn button">Generate Cars</button>
+        </div>`
+    }
+}
